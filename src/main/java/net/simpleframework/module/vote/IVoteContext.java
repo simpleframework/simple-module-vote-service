6b9f84929bc8872ad8b3a5e53cd8ccc3081ac2b4
simpleframework <cknet@126.com>
2013-11-15 12:12:13 +0800

    Signed-off-by: simpleframework <cknet@126.com>

--- src/main/java/net/simpleframework/module/vote/IVoteContext.java
+++ src/main/java/net/simpleframework/module/vote/IVoteContext.java
@@ -7,8 +7,7 @@ import net.simpleframework.module.vote.plugin.IVotePlugin;
 /**
  * Licensed under the Apache License, Version 2.0
  * 
- * @author 陈侃(cknet@126.com, 13910090885)
- *         https://github.com/simpleframework
+ * @author 陈侃(cknet@126.com, 13910090885) https://github.com/simpleframework
  *         http://www.simpleframework.net
  */
 public interface IVoteContext extends ICommonModuleContext {
